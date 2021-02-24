f92f0691cddf9f1069f5037211bbee454c54dba4
wilderlopez <wlopezelias@gmail.com>
2021-02-24 17:41:46 -0500

    test image aligment center

--- Sources/SwiftyChat/MessageCells/ImageCell.swift
+++ Sources/SwiftyChat/MessageCells/ImageCell.swift
@@ -53,7 +53,7 @@ public struct ImageCell<Message: ChatMessage>: View {
         .contentShape(CustomChatCorner(isCurrentUser: message.isSender))
         .shadow(radius: 1)
         .foregroundColor(.white)
-        .frame(alignment: .center)
+//        .frame(alignment: .center)
     }
     
     // MARK: - case Local Image
