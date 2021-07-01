cf9871439bb2ce95414fb23acb1355cee3c79577
wilderlopez <wlopezelias@gmail.com>
2021-07-01 12:04:51 -0400

    fix

--- Sources/SwiftyChat/MessageCells/ImageCell.swift
+++ Sources/SwiftyChat/MessageCells/ImageCell.swift
@@ -182,7 +182,7 @@ public struct ImageCell<Message: ChatMessage>: View {
             KFImage(url)
                 .onProgress(perform: { v1, v2 in
                     let frac : Double = Double(v1) / Double(v2)
-                    if downloadAmount <= 100 {
+                    if downloadAmount < 100 {
                     downloadAmount = frac * 100
                     print("v1: \(v1)")
                     print("v2: \(v2)")
