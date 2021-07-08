8a35c6dd881f76f3917e25ef3a3c2106275ae178
wilderlopez <wlopezelias@gmail.com>
2021-07-08 10:39:01 -0400

    [FIX] parameter cast

--- Sources/SwiftyChat/MessageCells/ImageCell.swift
+++ Sources/SwiftyChat/MessageCells/ImageCell.swift
@@ -239,7 +239,7 @@ public struct ImageCell<Message: ChatMessage>: View {
                 //only before start
                 Button {
                     print("start presigned download")
-                    let presignedRequest = MockMessages.RemoteResponseRow(triggerDownload: true, message: message)
+                    let presignedRequest = MockMessages.RemoteResponseRow(triggerDownload: true, message: message as? MockMessages.RemoteResponseRow.Message)
                     onRemoteResponse(presignedRequest)
                 } label: {
                     Image(systemName: "arrow.down")
